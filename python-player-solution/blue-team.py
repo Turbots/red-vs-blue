5ffe2016714c61c2dbfe4696d95bb632a3514faf
Timo Salm <tsalm@pivotal.io>
2019-09-25 18:30:44 +0200

    Align python and  java solution

--- python-player-solution/blue-team.py
+++ python-player-solution/blue-team.py
@@ -6,7 +6,9 @@ Author: Dieter Hubau
 """
 # !/usr/bin/env python
 import json
+import requests
 import os
+import random
 from datetime import timedelta
 
 import pika
@@ -34,13 +36,24 @@ except RuntimeError:
 @tl.job(interval=timedelta(milliseconds=1000))
 def send_message():
     if rabbit:
-        message = '{"thrower":{"id":10},"target":{"id":2}}'
+        refereeBaseUrl = os.getenv('BLUE_REFEREE_URL', 'http://localhost:8080')
 
-        channel.basic_publish(exchange='balls', routing_key='', body=message)
+        players = requests.get(url = '%s/score/ranking/' %refereeBaseUrl).json() 
+        teamRed = filter(lambda player: player['team'] == 'RED', players)
+        teamBlue = filter(lambda player: player['team'] == 'BLUE', players)
 
-        print(" [x] Sent %r" % message)
+        if len(teamRed) == 0 or len(teamBlue) == 0:
+            return 'Can\'t throw a ball when there are no players...'
+        else:
+            for player in teamBlue:
 
-        return 'Sent %r' % message
+                message = '{"thrower":{"id":%d},"target":{"id":%d}}' %(player["id"], random.choice(teamRed)["id"])
+
+                channel.basic_publish(exchange='balls', routing_key='', body=message)
+
+                print(" [x] Sent %r" % message)
+
+            return 'Sent %d messages' % len(teamBlue)
     else:
         return 'No Redis connection available!'
 
